7965c86e682101e88466cb7db202b65dd012092d
Sean Ni <nixiang915@hotmail.com>
2020-08-27 18:36:05 +0000

    finished tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,22 @@
 const request = require('request');
 const url = 'https://api.thecatapi.com/v1/images/search?breed_ids=';
 
-
-
-const inputHandler = function(input = process.argv[2]) {
-  let catId = input.slice(0, 4);
+const fetchBreedDescription = function(catId, callback) {
+  if (catId.length > 4) catId = catId.slice(0, 4);
   let catUrl = url + catId;
-  // console.log(catId);
   request(catUrl, (err, response, body) => {
     if (err) {
-      console.log(err);
+      callback(err, null);
       return;
     }
     // console.log(response.header);
     const cat = JSON.parse(body);
     if (cat.length > 0) {
-      console.log(cat[0].breeds[0].description);
+      callback(null, cat[0].breeds[0].description.trim());
     } else {
-      console.log('Cat not found :(');
+      callback({errid: 'Cat not found :('}, null);
     }
   });
 };
-inputHandler();
+
+module.exports = {fetchBreedDescription};
